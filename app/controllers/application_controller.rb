24e1670141b1d41cd1190076bf51988a5c065824
Noah Adams <nhadams10@uchicago.edu>
2021-02-07 23:43:06 +0000

    Currency home dashboard done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def from
+    
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @currencies = @symbols_hash.keys
+    
+    render({ :template => "dashboard_templates/forex.html.erb"})
+  end
+
 end
