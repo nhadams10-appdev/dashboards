edb10289006db7bf318899a9582126450856fbba
Noah Adams <nhadams10@uchicago.edu>
2021-02-08 00:00:47 +0000

    Did from currency step

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,39 @@
 class ApplicationController < ActionController::Base
 
-  def from
+  def start
     
     @raw_data = open("https://api.exchangerate.host/symbols").read
     @parsed_data = JSON.parse(@raw_data)
     @symbols_hash = @parsed_data.fetch("symbols")
 
     @currencies = @symbols_hash.keys
-    
+
     render({ :template => "dashboard_templates/forex.html.erb"})
+  end
+
+  def from
+
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @currencies = @symbols_hash.keys
+
+    @from = params.fetch("from_currency")
+    render({ :template => "dashboard_templates/from.html.erb"})
+  end
+
+  def to
+
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @currencies = @symbols_hash.keys
+
+    @from = params.fetch("from_currency")
+    @to = params.fetch("to_currency")
+    render({ :template => "dashboard_templates/from.html.erb"})
   end
 
 end
