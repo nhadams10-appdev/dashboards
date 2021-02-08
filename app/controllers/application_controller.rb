5379820de3749e97d85cfd1dbcef62a9d19937f7
Noah Adams <nhadams10@uchicago.edu>
2021-02-08 00:28:14 +0000

    Finished conversions dashboard!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,14 @@ class ApplicationController < ActionController::Base
 
     @from = params.fetch("from_currency")
     @to = params.fetch("to_currency")
-    render({ :template => "dashboard_templates/from.html.erb"})
+
+    @string = "https://api.exchangerate.host/convert?from=" + @from + "&to=" + @to
+    @raw_conversion = open(@string).read
+    @parsed_conversion = JSON.parse(@raw_conversion)
+    @conversion_hash = @parsed_conversion.fetch("info")
+    @conversion_rate = @conversion_hash.fetch("rate")
+    
+    render({ :template => "dashboard_templates/to.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get("/forex", { :controller => "application", :action => "start" })
   get("/forex/:from_currency", { :controller => "application", :action => "from" })
-  get("/forex/:from_currrency/:to_currency", { :controller => "application", :action => "to" })
+  get("/forex/:from_currency/:to_currency", { :controller => "application", :action => "to" })
 end
